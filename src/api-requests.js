5a216699981b9900e9304670bd68d116baeab658
ethanwhall389 <125170834+ethanwhall389@users.noreply.github.com>
2023-11-17 17:44:44 +0000

    Fix fetch cors bug

--- src/api-requests.js
+++ src/api-requests.js
@@ -1,6 +1,12 @@
 export default class Api {
     static async fetchWeather (query) {
-        const response = await fetch(`http://api.weatherapi.com/v1/forecast.json?key=4018e2169c904008a6b05305231711&q=${query}&days=3`, {mode: "cors"})
+        const response = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=4018e2169c904008a6b05305231711&q=${query}&days=3`, {
+            method: "GET",
+            mode: "cors",
+            credentials: "include",
+            headers: {
+                "Content-Type": "application/json"
+            })
         const data = await response.json();
         // console.log(data);
         return data
