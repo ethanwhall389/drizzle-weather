15016b4f2abb0504aa510de2d48d51dcbc8218d8
ethanwhall389 <125170834+ethanwhall389@users.noreply.github.com>
2023-11-20 20:20:09 +0000

    Begin ading dom manipulation

--- src/process-info.js
+++ src/process-info.js
@@ -8,25 +8,37 @@ export default class Process {
             },
             'tempC': data.current.temp_c,
             'tempF': data.current.temp_f,
+            'feelsLikeF': data.current.feelslike_f,
+            'feelsLikeC': data.current.feelslike_c,
+            'highF': data.forecast.forecastday[0].day.maxtemp_f,
+            'highC': data.forecast.forecastday[0].day.maxtemp_c,
+            'lowF': data.forecast.forecastday[0].day.mintemp_f,
+            'lowC': data.forecast.forecastday[0].day.mintemp_c,
             'condition': data.current.condition.text,
-            'precipitation': data.current.precip_in
+            'precipitation': data.current.precip_in,
+            'sunrise': data.forecast.forecastday[0].astro.sunrise,
+            'sunset': data.forecast.forecastday[0].astro.sunset
         }
     }
 
     static forecastWeather (data) {
         console.log(data);
         return {
+            'numOfDays': data.forecast.forecastday.length,
             'day0': {
                 'location': {
                     'name': data.location.name,
                     'region': data.location.region,
                     'country': data.location.country
                 },
+                'date': data.forecast.forecastday[0].date,
                 'sunrise': data.forecast.forecastday[0].astro.sunrise,
                 'sunset': data.forecast.forecastday[0].astro.sunset,
                 'isDark': data.forecast.forecastday[0].astro.is_sun_up,
                 'highC': data.forecast.forecastday[0].day.maxtemp_c,
                 'highF': data.forecast.forecastday[0].day.maxtemp_f,
+                'lowF': data.forecast.forecastday[0].day.mintemp_f,
+                'lowC': data.forecast.forecastday[0].day.mintemp_c,
                 'condition': data.forecast.forecastday[0].day.condition.text,
                 'rainChance': data.forecast.forecastday[0].day.daily_chance_of_rain,
                 'snowChance': data.forecast.forecastday[0].day.daily_chance_of_snow,
--- src/ui.js
+++ src/ui.js
@@ -1,6 +1,7 @@
 export default class UI {
     static currentWeather (weatherData) {
-        this.generalInfo(weatherData);
+        this.dispGeneralInfo(weatherData);
+        this.dispSpecificInfo(weatherData);
         console.log('weatherData:');
         console.log(weatherData);
         console.log(`${weatherData.location.name}, ${weatherData.location.region}`);
@@ -8,17 +9,103 @@ export default class UI {
     }
 
     static forecastWeather (weatherData) {
+        this.dispDaily(weatherData);
+        
         console.log(`${weatherData.day0.location.name}, ${weatherData.day0.location.region}`);
         console.log('forecast:')
         console.log(weatherData);
     }
 
-    static generalInfo (weatherData) {
-        const genInfoCont = document.querySelector('.general-info');
-        const conditions = document.createElement('p');
-        conditions.classList.add('conditions');
+    static dispGeneralInfo (weatherData) {
+        const conditions = document.querySelector('.conditions-cont .condition')
         conditions.textContent = weatherData.condition;
-        genInfoCont.appendChild(conditions);
-        console.log('CONDITIONS: ' + conditions);
+
+        const location = document.querySelector('.general-info .location');
+        location.textContent = `${weatherData.location.name}, ${weatherData.location.region}`;
+
+        const temp = document.querySelector('.general-info .temp');
+        temp.textContent = weatherData.tempF;
+
+        const high = document.querySelector('.general-info .high');
+        high.textContent = `High: ${weatherData.highF}\u00B0`;
+
+        const low = document.querySelector('.general-info .low');
+        low.textContent = `Low: ${weatherData.lowF}\u00B0`;
+    }
+
+    static dispSpecificInfo (weatherData) {
+        const sunrise = document.querySelector('.specific-info .sunrise');
+        sunrise.textContent = `Sunrise: ${weatherData.sunrise}`;
+
+        const sunset = document.querySelector('.specific-info .sunset');
+        sunset.textContent = `Sunset: ${weatherData.sunset}`;
+
+        const precipitation = document.querySelector('.specific-info .rain');
+        precipitation.textContent = `Chance of rain: ${weatherData.precipitation}%`;
+
+        const feelsLike = document.querySelector('.specific-info .index');
+        feelsLike.textContent = `Feels like: ${weatherData.feelsLikeF}\u00B0`;
+    }
+
+    static dispDaily (weatherData) {
+        const daysCont = document.querySelector('.days-cont');
+        for (let i = 0; i < weatherData.numOfDays; i++) {
+            const dayCont = document.createElement('div');
+            dayCont.classList.add('day-cont');
+
+            const day = document.createElement('p');
+            day.classList.add('day');
+            day.textContent = weatherData[`day${i}`].date;
+
+            const highLowCont = document.createElement('div');
+            highLowCont.classList.add('high-low');
+
+            const high = document.createElement('span');
+            high.classList.add('high');
+            high.textContent = `H: ${weatherData['day' + i].highF}\u00B0`;
+            
+            const low = document.createElement('span');
+            low.classList.add('low');
+            low.textContent = `L: ${weatherData['day' + i].lowF}\u00B0`;
+
+            const icon = this.createIcon(weatherData['day' + i].condition);
+
+            dayCont.appendChild(day);
+            highLowCont.appendChild(high);
+            highLowCont.appendChild(low);
+            dayCont.appendChild(highLowCont)
+            dayCont.appendChild(icon);
+            daysCont.appendChild(dayCont);
+        }
+        //use a for loop to go through each 
+    }
+
+    static createIcon (con) {
+        const icon = document.createElement('i');          
+
+        const low = con.toLowerCase();
+
+        if (low === 'sunny') {
+            icon.classList.add('fa-regular', 'fa-sun');
+        } else if (low === 'clear') {
+            icon.classList.add('fa-regular', 'fa-moon');
+        } else if (low === 'partly cloudy') {
+            icon.classList.add('fa-solid', 'fa-cloud-sun');
+        } else if (low === 'cloudy' || low === 'overcast') {
+            icon.classList.add('fa-solid', 'fa-cloud');
+        } else if (low === 'mist' || low.includes('drizzle') || low.includes('ice') || low.includes('sleet')) {
+            icon.classList.add('fa-solid', 'fa-cloud-rain');
+        } else if (low === 'fog') {
+            icon.classList.add('fa-solid', 'fa-smog');
+        } else if (low.includes('heavy') && low.includes('drizzle') || con.includes('rain')) {
+            icon.classList.add('fa-solid', 'fa-cloud-showers-heavy');
+        } else if (low.includes('rain')) {
+            icon.classList.add('fa-solid', 'fa-cloud-rain');
+        } else if (low.includes('snow') || low.includes('blizzard')) {
+            icon.classList.add('fa-regular', 'fa-snowflake');
+        } else if (low.includes('thundery')) {
+            icon.classList.add('fa-solid', 'fa-cloud-bolt');
+        }
+        return icon;
     }
 }
